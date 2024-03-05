d473785d5be9886c64e3bcd8c4598faba2b54941
Miskolczi Richárd <>
2024-03-05 11:35:10 +0100

    Adding files

---
+++ Gui_004_lab/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gui_004_lab
+{
+    
+    
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+    ObservableCollection<Soldier> soldiers;
+        public MainWindow()
+        {
+            InitializeComponent();
+            soldiers = new ObservableCollection<Soldier>()
+            {
+                new Soldier(){Type="Mariner", Power=30,Value=25,Vitality=25},
+                new Soldier(){Type="Pilot", Power=20,Value=15,Vitality=27},
+                new Soldier(){Type="Infantry", Power=25,Value=35,Vitality=21},
+                new Soldier(){Type="Sniper", Power=40,Value=45,Vitality=20},
+                new Soldier(){Type="Engineer", Power=20,Value=15,Vitality=20},
+            };
+            lbox_left.ItemsSource= soldiers;
+            
+        }
+
+        private void Label_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            string selected = (string)(sender as Label).Content;
+            var obj= soldiers.Select(t => t.Type == selected);
+            ;
+        }
+    }
+}
---
+++ Gui_004_lab/Soldier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gui_004_lab
+{
+    public class Soldier
+    {
+        public string Type { get; set; }
+        public int Power { get; set; }
+        public int Vitality { get; set; }
+        public int Value { get; set; }
+    }
+}
